f2069066d2184d44fcd48970a73170b073ba009f
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-05-17 01:53:55 +0300

    fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
-declare function $<K extends keyof HTMLElementTagNameMap>(selectors: K, parent?: ParentNode): HTMLElementTagNameMap[K] | null;
-declare function $<K extends keyof SVGElementTagNameMap>(selectors: K, parent?: ParentNode): SVGElementTagNameMap[K] | null;
-declare function $<E extends Element = Element>(selectors: string, parent?: ParentNode): E | null;
+export declare function $<K extends keyof HTMLElementTagNameMap>(selectors: K, parent?: ParentNode): HTMLElementTagNameMap[K] | null;
+export declare function $<K extends keyof SVGElementTagNameMap>(selectors: K, parent?: ParentNode): SVGElementTagNameMap[K] | null;
+export declare function $<E extends Element = Element>(selectors: string, parent?: ParentNode): E | null;
 
-declare function $a<K extends keyof HTMLElementTagNameMap>(selectors: K, parent?: ParentNode): NodeListOf<HTMLElementTagNameMap[K]>;
-declare function $a<K extends keyof SVGElementTagNameMap>(selectors: K, parent?: ParentNode): NodeListOf<SVGElementTagNameMap[K]>;
-declare function $a<E extends Element = Element>(selectors: string, parent?: ParentNode): NodeListOf<E>;
+export declare function $a<K extends keyof HTMLElementTagNameMap>(selectors: K, parent?: ParentNode): NodeListOf<HTMLElementTagNameMap[K]>;
+export declare function $a<K extends keyof SVGElementTagNameMap>(selectors: K, parent?: ParentNode): NodeListOf<SVGElementTagNameMap[K]>;
+export declare function $a<E extends Element = Element>(selectors: string, parent?: ParentNode): NodeListOf<E>;
